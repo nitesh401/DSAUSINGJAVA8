42d662c2bf1e2d416ae1384efce42c2d9e0c1897
nitesh401 <nitesh.rawal401@gmail.com>
2025-01-24 12:15:46 +0530

    r2

---
+++ wordLengthMap.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Main {
+    public static void main(String[] args) {
+ List<String> words = Arrays.asList("one", "two", "three");
+        Map<String, Integer> wordLengthMap = words.stream()
+            .collect(Collectors.toMap(word -> word, String::length));
+        String output = wordLengthMap.entrySet()
+            .stream()
+            .map(entry -> entry.getKey() + ":" + entry.getValue())
+            .collect(Collectors.joining(", ", "[", "]"));
+        System.out.println(output);
+    }
+}
